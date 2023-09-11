f0d45e2ad4053824947927036fa67b271ea9011a
robsontaveira <robsontaveira1@gmail.com>
2023-09-11 20:11:08 -0300

    Create cap06_03.py

---
+++ python/DSA/cap06/cap06_03.py
@@ -0,0 +1,19 @@
+#PACOTES E MÓDULOS EM PYTHON
+
+#Importando um pacote Python
+import numpy
+
+#Verificando todos os métodos e atributos disponiveis no pacote
+print('\n',dir(numpy))
+
+#Usando o método que calcula a raiz quadrada de um numero
+print('\n',numpy.sqrt(25))
+
+#Importando apenas um dos métodos do pacote Numpy
+from numpy import sqrt
+print('\nA raiz quadrada de 9 é: ',sqrt(9))   #usando o método
+
+
+
+
+print('\n','Fim do código')
