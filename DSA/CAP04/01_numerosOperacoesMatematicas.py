70316c72b46cb92db8e719745f4e430a3825cd87
robsontaveira <143971627+robsontaveira@users.noreply.github.com>
2024-01-20 15:35:25 -0300

    Add files via upload

---
+++ DSA/CAP04/01_numerosOperacoesMatematicas.py
@@ -0,0 +1,22 @@
+# -- Operações Matemáticas --
+
+# multiplicação
+print(2*2)
+# exponencial
+print(4**2)
+print(pow(4,2))
+# divisão
+print(10/2)
+# módulo (resto da divisão)
+print(10%3)
+# divisão parte inteira
+print(10//3)
+
+# -- Algumas fuñções --
+
+# função type(dado), verifica o tipo do dado ou variável
+print(type('a'))
+print(type(10))
+print(type(10.1))
+# função round(number, ndigits)
+print(round(3.1415,2))
