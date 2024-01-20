3eedd66cb1e8a158352b406553ae0cb04bd44aef
robsontaveira <143971627+robsontaveira@users.noreply.github.com>
2024-01-20 15:38:40 -0300

    Add files via upload

---
+++ DSA/CAP04/02_variaveisOperadores.py
@@ -0,0 +1,17 @@
+# -- Declarar variáveis --
+
+# declaração de variável
+var = 1     # atribui o valor 1 para a variável var
+print(var)  #imprime na tela o valor de var
+
+# declaração múltiplas
+pessoa1, pessoa2, pessoa3 = 'Zé','Ana','Bia'
+print(pessoa1,'\n',pessoa2,'\n',pessoa3)
+
+# concatenação de variáveis
+nome = 'Bob'
+sobrenome = 'Marley'
+fullname = nome+' '+sobrenome
+print(fullname)
+
+
